822e197bacd2d74d23f2593cedf8d626e9817712
calvinmckbeav <92180625+calvinmckbeav@users.noreply.github.com>
2024-04-10 10:54:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-window.onload = function() {
-    fetchJOLTSData();
-};
-const API_KEY = '312e520ae86546fb86b64e51a4e7e7c8';
+const API_KEY = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
 
 function fetchJOLTSData() {
-    const url = `https://api.bls.gov/publicAPI/v2/timeseries/data/JTU000000000000000JOL?registrationkey=${API_KEY}&startyear=2018&endyear=2030`;
+    const url = `https://api.bls.gov/publicAPI/v2/timeseries/data/JTS000000000000000JOL?registrationkey=${API_KEY}`;
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
         const series = data.Results.series[0];
-        let filteredData = series.data.filter(item => parseInt(item.year) >= 2013 && item.period === 'M01');
-        const labels = filteredData.map(item => `${item.year}`);
-        const values = series.data.map(item => parseInt(item.value));
+        const monthlyData = series.data.filter(item => parseInt(item.year) >= 2013);
+        const labels = monthlyData.map(item => {
+            if (item.period === 'M01') {
+                return `${item.year}`;
+            } else {
+                return '';
+            }
+        });
+        const values = monthlyData.map(item => parseInt(item.value));
 
         createChart(labels, values);
     })
@@ -26,10 +29,10 @@ function createChart(labels, values) {
     const chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: labels.reverse(), // Reverse labels to show from oldest to newest
+            labels: labels,
             datasets: [{
                 label: 'Job Openings',
-                data: values.reverse(), // Reverse values to match labels order
+                data: values,
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 1,
